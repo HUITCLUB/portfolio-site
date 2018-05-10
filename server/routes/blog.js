15d7bc830dbaf6e0663cc56d0e5c5e6e8cd2823c
Yewken <cfr2ak@gmail.com>
2018-05-10 18:31:49 +0900

    fix top, remove block annotation head.pug foot.pug

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HUIT' });
+  res.render('blog', { title: 'HUIT-blog' });
 });
 
 module.exports = router;
--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -6,10 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('portfolio', { title: 'HUIT-portfolio' });
 });
 
-
-// TODO: make user list to save portfolio each
-router.get('/users', function(req, res, next) {
-  res.render('userlist', { title: 'HUIT-portfolio' });
-});
-
 module.exports = router;
