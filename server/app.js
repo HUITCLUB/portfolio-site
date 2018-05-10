452560e34c8546be522ecff6ad224e0e8c2849ed
Yewken <cfr2ak@gmail.com>
2018-05-10 18:03:29 +0900

    add blog path

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var portfolio = require('./routes/portfolio');
+var blog = require('./routes/blog');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/portfolio', portfolio);
+app.use('/blog', blog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
