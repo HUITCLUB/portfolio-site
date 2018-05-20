97f9dbd9c724be3a23ff838405b940bd3d3fbd2f
Yewken <cfr2ak@gmail.com>
2018-05-20 13:58:47 +0900

    port page

--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const assert = require('assert');
+
+MongoClient.connect('mongodb://127.0.0.1:27017/portfolioes', (err, db) => {
+
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('portfolio', { title: 'HUIT-portfolio' });
+});
+
+router.get('/ken', function(req, res, next) {
+  res.render('port-page', { title: 'portfolio-ken' });
 });
 
 module.exports = router;
