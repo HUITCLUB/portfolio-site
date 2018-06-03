64b6f8d610eb8406ab73072b58060c92c21d102e
cfr2ak <cfr2ak@gmail.com>
2018-06-03 13:35:54 +0900

    database(mongo) portfolio page interpolation

--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -4,32 +4,54 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 const url = 'mongodb://127.0.0.1:27017';
-const dbname = 'huitclub';
+const dbName = 'huitclub';
 
-const findPortfolioes = function(db, cb) {
-  const collection = db.collection('portfolioes');
-  collection.find({}).toArray(function err, ports {
+const find = function(db, cond, cb) {
+  const collection = db.collection('portfolio');
+  collection.find(cond).toArray(function(err, data) {
     assert.equal(null, err);
-    cb(ports);
+    cb(data);
   });
-}
+};
 
-MongoClient.connect(url, (err, client) => {
-  assert.equal(null, err);
-
-  const db = client.db(dbName);
-
-  client.close();
-});
-
+// markdown renderer
+const md = require('markdown-it')();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('portfolio', { title: 'HUIT-portfolio' });
+  MongoClient.connect(url, (err, client) => {
+    assert.equal(null, err);
+    const db = client.db(dbName);
+
+    find(db, {}, function(data) {
+      data.map(x => delete x._id);
+      data.map(x => delete x.page);
+      for (var i = 0; i < data.length; i++) {
+        data[i].path = "/portfolio".concat(data[i].path);
+      }
+
+      res.render('portfolio', { title: 'HUIT-portfolio', data: {val: data} });
+      client.close();
+    });
+  });
+
 });
 
-router.get('/ken', function(req, res, next) {
-  res.render('port-page', { title: 'portfolio-ken' });
+router.get('/yoonken', function(req, res, next) {
+  MongoClient.connect(url, (err, client) => {
+    assert.equal(null, err);
+    const db = client.db(dbName);
+
+    find(db, {'path': '/yoonken'}, function(data) {
+      assert.equal(1, data.length);
+      data = data[0];
+      delete data._id;
+      data.page = md.render(data.page);
+
+      res.render('port-page', { title: 'portfolio-ken', data: data});
+      client.close();
+    });
+  });
 });
 
 module.exports = router;
