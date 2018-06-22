61de5e4051aeeba86fea32e933545442f05f829d
Yewken <cfr2ak@gmail.com>
2018-06-22 18:18:41 +0900

    tmp

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -27,11 +27,7 @@ router.get('/', function(req, res, next) {
     find(db, {}, function(data) {
       data.map(x => delete x._id);
       data.map(x => delete x.page);
-      for (var i = 0; i < data.length; i++) {
-        data[i].path = "/portfolio".concat(data[i].path);
-      }
-
-      res.render('portfolio', { title: 'HUIT-portfolio', data: {val: data} });
+      
       client.close();
     });
   });
@@ -39,28 +35,28 @@ router.get('/', function(req, res, next) {
   res.render('blog', { title: 'HUIT-blog' });
 });
 
-router.get('/:category/:date/:title', function(req, res, next) {
-  MongoClient.connect(url, (err, client) => {
-    // TODO: need to change assert statement into error handling function in general
-    assert.equal(null, err);
-
-    const category = req.params.category;
-    const date = req.params.date;
-    const title = req.params.title;
-
-    const db = client.db(dbName);
-
-    find(db, {
-      'date': date,
-      'category': category,
-      'title': title,}, function(data) {
-      data.map(x => delete x._id);
-      data.map(x => delete x.page);
-
-      res.render('blog-page', { title: 'HUIT-portfolio', data: {val: data} });
-      client.close();
-    });
-  });
-});
+// router.get('/:category/:date/:title', function(req, res, next) {
+//   MongoClient.connect(url, (err, client) => {
+//     // TODO: need to change assert statement into error handling function in general
+//     assert.equal(null, err);
+//
+//     const category = req.params.category;
+//     const date = req.params.date;
+//     const title = req.params.title;
+//
+//     const db = client.db(dbName);
+//
+//     find(db, {
+//       'date': date,
+//       'category': category,
+//       'title': title,}, function(data) {
+//       data.map(x => delete x._id);
+//       data.map(x => delete x.page);
+//
+//       res.render('blog-page', { title: 'HUIT-portfolio', data: {val: data} });
+//       client.close();
+//     });
+//   });
+// });
 
 module.exports = router;
